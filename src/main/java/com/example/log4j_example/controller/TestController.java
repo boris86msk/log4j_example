03bdf80f0505b7ec868e5c6983d30e5f36b2000a
boris86msk <boris86msk@yandex.ru>
2023-05-13 10:23:21 +0300

    mess1

--- src/main/java/com/example/log4j_example/controller/TestController.java
+++ src/main/java/com/example/log4j_example/controller/TestController.java
@@ -1,23 +1,22 @@
 package com.example.log4j_example.controller;
 
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.logging.Logger;
 
 
 @RestController
 public class TestController {
 
-    //private static final Logger LOGGER = LogManager.getLogger(TestController.class);
+    private static final Logger LOGGER = LogManager.getLogger(TestController.class);
 
-    private static final Logger logger = Logger.getLogger(TestController.class.getName());
+    //private static final Logger logger = Logger.getLogger(TestController.class.getName());
 
 
     @GetMapping("/")
     public String testLogger() {
-        logger.info("Проверяем работу логгера ");
+        LOGGER.info("Проверяем работу логгера ");
         return "Ok";
     }
 }
